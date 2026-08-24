14541d15401702d37f1b796592601600b0dae3b4
Daxcr <daxaroodles@gmail.com>
2026-08-24 22:45:46 +1200

    Middleware wip

--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -1,8 +1,8 @@
-﻿Server server = new Server
+﻿Server server = new Server("TestApp")
 {
     Port = 5555,
     Host = "127.0.0.1",
-    MaxRequestSize = 1024 * 1024 * 3
+    MaxRequestSize = 1024 * 1024 * 3,
 };
 server.AcceptingConnections = true;
 
--- ExampleProject/Server.cs
+++ ExampleProject/Server.cs
@@ -1,35 +1,32 @@
 using Rose.Net;
+using Rose.Net.Middleware.RateLimiting;
 
 public class Server : GenericServer
 {
-    public Server()
+    public Server(string name = "APP", bool silentInit = false)
+        : base(name, silentInit)
     {
         OnClientConnect += OnConnection;
         OnClientRequest += OnRequest;
         OnClientClose += OnClose;
 
         AddRoute(Route.Get("/api/test", TestAPI));
+        AddMiddleware<FixedWindow>();
     }
     public async Task TestAPI(Client client)
     {
-        Console.WriteLine($"Route fired by {client.IP}");
+        Debug.Log($"Route fired by {client.IP}");
         await client.Respond302("https://dax.cr/");
     }
-    public async Task OnConnection(Client client)
-    {
-        Console.WriteLine($"Connection from {client.IP}");
-    }
+    public async Task OnConnection(Client client) => Debug.Log($"Connection from {client.IP}");
     public async Task OnRequest(Client client)
     {
-        Console.WriteLine($"Request from {client.IP}: {client.Method} {client.Path} {client.Version}");
+        Debug.Log($"Request from {client.IP}: {client.Method} {client.Path} {client.Version}");
 
         await ApplyRoutes(client);
         if (client.RouteMatched) return;
 
         await client.RespondStatic();
     }
-    public async Task OnClose(Client client)
-    {
-        Console.WriteLine($"Client {client.IP} has expired");
-    }
+    public async Task OnClose(Client client) => Debug.Log($"Client {client.IP} connection has closed");
 }
